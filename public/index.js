c16e345d15d24068eb3a6cb76591e737115b9e95
Martin-Alexander <martingianna@gmail.com>
2018-06-02 19:44:18 -0400

    html and front-end javascript to send and recieve messages from sever

--- public/index.js
+++ public/index.js
@@ -1,9 +1,34 @@
+const inputField = document.getElementById("input-field");
+const sendButton = document.getElementById("send-button");
+const messageList = document.getElementById("message-list");
+
 const websocket = new WebSocket("ws://localhost:3000", "echo-protocol");
 
+const sendMessage = () => {
+  const userInput = inputField.value;
+  inputField.value = "";
+
+  websocket.send(userInput);
+};
+
+const recieveMessage = (message) => {
+  messageList.insertAdjacentHTML("afterbegin", `
+    <li>${message}</li>
+  `)
+};
+
 websocket.onopen = (event) => {
-  websocket.send("Hey!");
+  document.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13 && inputField === document.activeElement) {
+      sendMessage();
+    }
+  });
+  
+  sendButton.addEventListener("click", () => {
+    sendMessage();
+  });  
 };
 
 websocket.onmessage = (event) => {
-  console.log(event.data);
+  recieveMessage(event.data);
 }
